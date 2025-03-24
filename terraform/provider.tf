cbcd1b6aff6528bfaa19e0e47015f2112dfac99c
Gax <gax.theodorio@crowdstrike.com>
2025-03-24 10:09:37 -0400

    Update provider config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project     = var.deployment_project_id
-  region      = "us-central1"
+  region      = var.location
   billing_project       = var.deployment_project_id
   user_project_override = true
 }
